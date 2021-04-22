ee98162312d632f49e8251cfcbfe26ba4fdcf072
Alexsandra oliveira de souza <alexsa.aods1@gmail.com>
2021-04-21 22:48:18 -0400

    pau no seeder do banco

--- resources/views/meus_dados/edita_conta.blade.php
+++ resources/views/meus_dados/edita_conta.blade.php
@@ -5,7 +5,7 @@
 <div class="card text-dark bg-light mb-3">
     <div class="card-header">
         <h6 class="card-title">
-            <i class="fas fa-calculator me-2"></i>Adicionar dados bancários
+            <i class="fas fa-calculator me-2"></i>Editar dados bancários
         </h6>
     </div>
     <div class="card-body">
@@ -44,28 +44,28 @@
                 <div class="row mt-3">
                     <div class="col-2">
                         <div class="form-floating mb-3">
-                            <input type="text" class="form-control" name="agencia" id="agencia" placeholder="agencia" autocomplete="off" value="{{(old('agencia'))}}">
+                            <input type="text" class="form-control" name="agencia" id="agencia" placeholder="agencia" autocomplete="off" value="{{(old('agencia' , $conta->agencia))}}">
                             <label for="agencia">Agência</label>
                         </div>
                     </div>
 
                     <div class="col-3">
                         <div class="form-floating mb-3">
-                            <input type="text" class="form-control" name="numero" id="numero" placeholder="numero" autocomplete="off" value="{{(old('numero'))}}">
+                            <input type="text" class="form-control" name="numero" id="numero" placeholder="numero" autocomplete="off" value="{{(old('numero' , $conta->numero))}}">
                             <label for="numero">Número</label>
                         </div>
                     </div>
 
                     <div class="col-2">
                         <div class="form-floating mb-3">
-                            <input type="text" class="form-control" name="operacao" id="operacao" placeholder="operacao" autocomplete="off" value="{{(old('operacao'))}}">
+                            <input type="text" class="form-control" name="operacao" id="operacao" placeholder="operacao" autocomplete="off" value="{{(old('operacao' , $conta->operacao))}}">
                             <label for="operacao">Operação</label>
                         </div>
                     </div>
 
                     <div class="col-5">
                         <div class="form-floating mb-3">
-                            <input type="text" class="form-control" name="pix" id="pix" placeholder="pix" autocomplete="off" value="{{(old('pix'))}}">
+                            <input type="text" class="form-control" name="pix" id="pix" placeholder="pix" autocomplete="off" value="{{(old('pix' , $conta->pix))}}">
                             <label for="pix">Chave PIX</label>
                         </div>
                     </div>
@@ -75,7 +75,7 @@
                 <div class="row">
                     <div class="col text-end">
                         <a href="#" class="btn btn-outline-secondary">Cancelar</a>
-                        <input class="btn btn-outline-success" type="submit" value="Inserir">
+                        <input class="btn btn-outline-success" type="submit" value="Salvar">
                     </div>
                 </div>
             </form>
