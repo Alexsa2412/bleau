e6b2201c7c0d47d923791610645c8e8325a701ff
Bruno da Silva Ferreira <bs.ferreiras@gmail.com>
2021-05-06 14:26:35 -0400

    corrindo o nome da tabela convites para convite

--- app/Models/Convite/Convite.php
+++ app/Models/Convite/Convite.php
@@ -9,6 +9,6 @@ class Convite extends Model
 {
     use HasFactory;
 
-    protected $table = 'convites';
+    protected $table = 'convite';
     protected $fillable = ['email_do_convidado', 'nome_do_convidado', 'codigo_do_convite', 'pessoa_id'];
 }
