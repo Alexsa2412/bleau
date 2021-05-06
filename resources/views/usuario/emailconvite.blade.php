e565ff22ca65d5758e8ef7fcc7c4e924d9bbff8d
Alexsandra oliveira de souza <alexsa.aods1@gmail.com>
2021-05-06 16:38:14 -0400

    email convite

---
+++ resources/views/usuario/emailconvite.blade.php
@@ -0,0 +1,20 @@
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+
+    <title>Convite</title>
+</head>
+    <h1 style="font-family: Arial, Helvetica, sans-serif; text-align: center">Convite</h1>
+<body style="background-color: #F0F8FF; font-family: Arial, Helvetica, sans-serif">
+    <div>
+        Caro Sr(a) <b>(nome do convidado)</b>, você foi convidado(a) pelo(a) <b>(nome de quem convidou)</b>, a se tornar membro do Conselho Azul.
+        Clique no botão abaixo para efetivar sua participação.
+    </div>
+    </p>
+    <button type="button" name="Enviar" value="" style="padding-top:10px; padding-bottom:10px; padding-left: 8px; padding-right 20px; cursor: pointer; background-color: DodgerBlue; color:white; border-style:none">Enviar</button>
+
+</body>
+</html>
